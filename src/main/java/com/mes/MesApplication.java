123ddee70ff49cef7ba22d6cf73a421738964a46
jeongjaebong <rkqkdrn@naver.com>
2026-04-13 00:04:45 +0900

    fix: JVM 및 Jackson 타임존 Asia/Seoul 고정
    EC2 기본 UTC 환경에서 자정 스케줄러(0 0 0 * * *)가 KST 09:00에 실행되는 문제 수정

--- src/main/java/com/mes/MesApplication.java
+++ src/main/java/com/mes/MesApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 public class MesApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(MesApplication.class, args);
     }
 }
